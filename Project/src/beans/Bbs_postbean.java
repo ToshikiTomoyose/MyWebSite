bc0f29c248ca89f596a07aefff412148023b04de
ToshikiTomoyose <sf7fantazy7assaltsk@gmail.com>
2018-02-15 10:34:59 +0900

    全ジャワ豆作成

---
+++ Project/src/beans/Bbs_postbean.java
@@ -0,0 +1,94 @@
+package beans;
+import java.io.Serializable;
+
+public class Bbs_postbean implements Serializable {
+
+	private int id ;
+	private String text;
+	private String post_photo;
+	private int user_id;
+	private String profile_photo;
+	private int thread_id;
+	private String create_date;
+	private String update_date;
+
+	public Bbs_postbean () {}
+
+	public Bbs_postbean (int id, String text, String post_photo, int user_id, String profile_photo, int thread_id, String create_date, String update_date) {
+
+		this.id = id;
+		this.text = text;
+		this.post_photo = post_photo;
+		this.user_id = user_id;
+		this.profile_photo = profile_photo;
+		this.thread_id = thread_id;
+		this.create_date = create_date;
+		this.update_date = update_date;
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String getPost_photo() {
+		return post_photo;
+	}
+
+	public void setPost_photo(String post_photo) {
+		this.post_photo = post_photo;
+	}
+
+	public int getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getProfile_photo() {
+		return profile_photo;
+	}
+
+	public void setProfile_photo(String profile_photo) {
+		this.profile_photo = profile_photo;
+	}
+
+	public int getThread_id() {
+		return thread_id;
+	}
+
+	public void setThread_id(int thread_id) {
+		this.thread_id = thread_id;
+	}
+
+	public String getCreate_date() {
+		return create_date;
+	}
+
+	public void setCreate_date(String create_date) {
+		this.create_date = create_date;
+	}
+
+	public String getUpdate_date() {
+		return update_date;
+	}
+
+	public void setUpdate_date(String update_date) {
+		this.update_date = update_date;
+	}
+
+}
---
+++ Project/src/beans/Bbs_threadbean.java
@@ -0,0 +1,102 @@
+package beans;
+import java.io.Serializable;
+
+public class Bbs_threadbean implements Serializable {
+	private int id;
+	private String title;
+	private String maintext;
+	private int category_id;
+	private String thread_photo;
+	private int user_id;
+	private String create_date;
+	private String update_date;
+	private String profile_photo;
+
+	public Bbs_threadbean () {}
+
+	public Bbs_threadbean (int id, String title, String maintext, int category_id, String thread_photo, int user_id, String create_date, String update_date, String profile_photo) {
+
+		this.id = id;
+		this.title = title;
+		this.maintext = maintext;
+		this.category_id = category_id;
+		this.thread_photo = thread_photo;
+		this.user_id = user_id;
+		this.create_date = create_date;
+		this.update_date = update_date;
+		this.profile_photo = profile_photo;
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getMaintext() {
+		return maintext;
+	}
+
+	public void setMaintext(String maintext) {
+		this.maintext = maintext;
+	}
+
+	public int getCategory_id() {
+		return category_id;
+	}
+
+	public void setCategory_id(int category_id) {
+		this.category_id = category_id;
+	}
+
+	public String getThread_photo() {
+		return thread_photo;
+	}
+
+	public void setThread_photo(String thread_photo) {
+		this.thread_photo = thread_photo;
+	}
+
+	public int getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getCreate_date() {
+		return create_date;
+	}
+
+	public void setCreate_date(String create_date) {
+		this.create_date = create_date;
+	}
+
+	public String getUpdate_date() {
+		return update_date;
+	}
+
+	public void setUpdate_date(String update_date) {
+		this.update_date = update_date;
+	}
+
+	public String getProfile_photo() {
+		return profile_photo;
+	}
+
+	public void setProfile_photo(String profile_photo) {
+		this.profile_photo = profile_photo;
+	}
+}
